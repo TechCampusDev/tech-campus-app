3fef1a2120446e783c3784af1972dbb420d9a282
Yoshiaki Oshima <oshima@buildman.co.jp>
2023-11-21 15:17:08 +0900

    タイトル修正

--- tcapp/lib/main.dart
+++ tcapp/lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp.router(
-      title: 'TechCampus Web',
+      title: 'TechCampusApp',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
